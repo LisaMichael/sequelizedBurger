d2fcff896c449c1289a284aa98380f34602715a4
Lisa <lisa.michael@hotmail.com>
2020-01-07 23:48:12 -0500

    modified burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,33 @@
 //requires our orm 
 //cotnains all the methods that will be used in order to modify the orms for use with the ///   burgers database 
 
-var orm = require('../config/orm.js');
+// var orm = require('../config/orm.js');
 
-var burger = {
-    all: function (cb){
-        orm.all('burgers', function (res) {
-            cb(res);
-        })
-    },
+// var burger = {
+//     all: function (cb){
+//         orm.all('burgers', function (res) {
+//             cb(res);
+//         })
+//     },
 
-    update: function (id, cb) {
-        orm.update('burgers', id,cb);
-    },
-    create: function(name,cb){
-        orm.create('burgers',name,cb);
-    }
-}
+//     update: function (id, cb) {
+//         orm.update('burgers', id,cb);
+//     },
+//     create: function(name,cb){
+//         orm.create('burgers',name,cb);
+//     }
+// }
 
+
+module.exports = (sequelize, DataTypes) => {
+    var Burger = sequelize.define('Burger', {
+      burger_name: DataTypes.STRING,
+      devoured:{
+         type:DataTypes.BOOLEAN,
+         defaultValue: false
+      }
+    });
+    return Burger;
+  };
+  
 module.exports = burger; 
